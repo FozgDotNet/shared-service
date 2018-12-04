578ae3e19a78952f2b2feb22d433c6eeb2b1e252
fozg <phongdaohong@gmail.com>
2018-12-04 16:02:38 +0700

    add api tests

---
+++ src/tests/userProfile_api.test.js
@@ -0,0 +1,32 @@
+
+process.env.RUNNING = 'test';
+var UserProfileModel = require("../models/UserProfile.model");
+
+var expect = require('chai').expect;
+var chaiHttp = require('chai-http');
+var chai = require('chai');
+var should = chai.should();
+chai.use(chaiHttp);
+
+var server = require('../server');
+
+it('should list SIGNLE userProfiles on /api/userProfile/<id> GET', function(done) {
+  var _user = new UserProfileModel({email: 'test@gmail.com', name: 'testing'});
+  _user.save(function (err, _doc) {
+    chai.request(server)
+      .get('/api/userProfile/'+ _doc._id)
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('_id');
+        res.body.should.have.property('name');
+        res.body.should.have.property('email');
+        res.body.email.should.equal('test@gmail.com');
+        res.body.name.should.equal('testing');
+        done();
+      });
+  });
+
+ 
+});
