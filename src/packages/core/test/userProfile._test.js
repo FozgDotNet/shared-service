d7a80ca4aa8fe880d760aff2a94459f0066a3b8c
fozg <phongdaohong@gmail.com>
2018-12-04 16:04:30 +0700

    rename

---
+++ src/packages/core/test/userProfile._test.js
@@ -0,0 +1,35 @@
+
+// unuse
+
+// var expect = require('chai').expect;
+// process.env = 'test';
+
+// var ss = require('../../../app');
+
+// var id = null;
+// it('should add userProfile success before continute', function (done) {
+//   new ss.core.UserProfile({email : 'test@gmail.com'}).save().then(doc => {
+//     console.log(doc)
+//     _id = doc._id;
+//     expect(doc).to.be.an('object')
+//     done()
+//   });  
+// })
+
+// it('should get userinfo by call findByEmail ', function(done) {
+//   new ss.core.UserProfile().findByEmail('test@gmail.com').then(doc => {
+//     console.log(doc)
+//     // expect( res ).to.equal( null );
+//     expect(doc).to.be.an('object')
+//     done();
+//   })
+// });
+
+// it('should get userinfo by call findById ', function(done) {
+//   new ss.core.UserProfile().findByEmail('test@gmail.com').then(doc => {
+//     console.log(doc)
+//     // expect( res ).to.equal( null );
+//     expect(doc).to.be.an('object')
+//     done();
+//   })
+// });
